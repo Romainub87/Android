b63d4258c67e2be65c50cb50c9e1112d89c8a8ca
Romainub87 <106332313+Romainub87@users.noreply.github.com>
2023-02-20 16:34:10 +0100

    fin exo 9

--- exo9 Android/app/src/main/java/com/example/exo9android/MainActivity.java
+++ exo9 Android/app/src/main/java/com/example/exo9android/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +15,23 @@ public class MainActivity extends AppCompatActivity {
     private Button appel;
     private Button web;
     private Button map;
+    private EditText inputTel;
+    private EditText url;
+    private EditText longitude;
+    private EditText latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputTel = findViewById(R.id.inputTel);
+        url = findViewById(R.id.url);
+        longitude = findViewById(R.id.longitude);
+        latitude = findViewById(R.id.latitude);
 
         sms = findViewById(R.id.sms);
         sms.setOnClickListener(view -> {
-            Uri uri = Uri.parse("smsto:0752642686");
+            Uri uri = Uri.parse("smsto:"+ inputTel.getText());
             Intent SMS = new Intent(Intent.ACTION_SENDTO, uri);
             SMS.putExtra("sms body", "SMS text");
             startActivity(SMS);
@@ -30,28 +39,28 @@ public class MainActivity extends AppCompatActivity {
         });
         mms = findViewById(R.id.mms);
         mms.setOnClickListener(view -> {
-            Uri uri = Uri.parse("mmsto:0752642686");
+            Uri uri = Uri.parse("mmsto:" + inputTel.getText());
             Intent MMS = new Intent(Intent.ACTION_SENDTO, uri);
             startActivity(MMS);
             printName(mms);
         });
         appel = findViewById(R.id.appel);
         appel.setOnClickListener(view -> {
-            Uri uri = Uri.parse("tel:0752642686");
+            Uri uri = Uri.parse("tel:" + inputTel.getText());
             Intent tel = new Intent(Intent.ACTION_DIAL, uri);
             startActivity(tel);
             printName(appel);
         });
         web = findViewById(R.id.web);
         web.setOnClickListener(view -> {
-            Uri webpage = Uri.parse("http://romainub87.github.io/Romain");
+            Uri webpage = Uri.parse(String.valueOf(url.getText()));
             Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
             startActivity(webIntent);
             printName(web);
         });
         map = findViewById(R.id.map);
         map.setOnClickListener(view -> {
-            Uri location = Uri.parse("geo:0,0?q=1600+Amphitheatre+Parkway,+Mountain+View,+CA+94043"); // z param is zoom level
+            Uri location = Uri.parse("geo:" + latitude.getText() + "," + longitude.getText() ); // z param is zoom level
             Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
             startActivity(mapIntent);
             printName(map);
